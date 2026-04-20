872293538c6e51a9d79cb9cffdfef6fefdeada4a
daniel_alves_pinheiro <danielalvespinheiro467@gmail.com>
2026-04-20 14:26:39 -0400

    Criei o arquivo requirements.txt para criação do .venv, mais detalhes na descrição do PR.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.analyze import router as analyze_router
-from routes.history import router as history_router
+# from routes.analyze import router as analyze_router
+# from routes.history import router as history_router
 from database.db import init_db
+from contextlib import asynccontextmanager
 
 app = FastAPI(
     title="GuarAssist API",
@@ -18,12 +19,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Servidor rodando... ")
     init_db()
+    yield
+    print("Encerrando servidor...")
 
-app.include_router(analyze_router, prefix="/api")
-app.include_router(history_router, prefix="/api")
+# app.include_router(analyze_router, prefix="/api")
+# app.include_router(history_router, prefix="/api")
 
 @app.get("/")
 def root():
